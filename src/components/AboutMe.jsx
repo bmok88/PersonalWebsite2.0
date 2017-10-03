cf20460f3c21d7f03f37475912b64d5026180d79
Brendan Mok <brendanmok@Brendans-MacBook-Pro.local>
2017-10-03 04:55:47 -0700

    Add first paragraph of about me

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -2,8 +2,23 @@ import React from 'react';
 
 const AboutMe = () => (
   <section>
-    <h1>Welcome To My Corner of The Universe!</h1>
-    <h4>Brendan here. I'm a detail-oriented software engineer who leverages the most modern technologies to create UX centered applications rich in aesthetics and usability.</h4>
+    <h1>
+      Welcome To My Corner of The Universe!
+    </h1>
+    <h4>
+      Brendan here. I'm a detail-oriented software engineer who leverages modern
+      technologies to create aesthetic, performant applications that deliver a
+      rich UX.
+    </h4>
+    <p>
+      I discovered my passion for programming mainly through circumstance with a
+      healthy injection of curiosity. As a former pre-med undergrad, I know full
+      well the struggles and stick-your-face-in-the-book commitments that engulf
+      the journey from prospective med school student to MD (major, major props
+      to those who made it). My lack of interest in medicine and sense of
+      urgency in getting my shit together led me to my first few lines of code, and the rest is history.
+    </p>
+    <p />
   </section>
 );
 
