1c065232c30dcdb2109989c738591a194842d4f0
Brendan Mok <brendanmok@Brendans-MacBook-Pro.local>
2017-10-02 21:20:26 -0700

    Change header

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const NavBar = () => (
   <nav>
     <a href="">About Me</a>
-    <a href="">Software Interests</a>
+    <a href="">What I Do</a>
     <a href="">Skills</a>
     <a href="">Experience</a>
     <a href="">Contact</a>
--- src/components/Website.jsx
+++ src/components/Website.jsx
@@ -6,6 +6,7 @@ const Website = () => (
   <div>
     <header>
       <h1>Brendan Mok</h1>
+      <h4>Software Engineer</h4>
       <NavBar />
     </header>
   </div>
