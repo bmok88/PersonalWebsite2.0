018663cb3d49822fd5b11d218a6815f5990580fe
Brendan Mok <brendanmok@Brendans-MacBook-Pro.local>
2017-10-03 05:37:22 -0700

    Finish about me paragraphs

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -16,9 +16,25 @@ const AboutMe = () => (
       well the struggles and stick-your-face-in-the-book commitments that engulf
       the journey from prospective med school student to MD (major, major props
       to those who made it). My lack of interest in medicine and sense of
-      urgency in getting my shit together led me to my first few lines of code, and the rest is history.
+      urgency in getting my shit together led me to my first few lines of code,
+      and the rest is history.
     </p>
-    <p />
+    <p>
+      My primary focus is the front end because I'm naturally a perfectionist
+      and I take pride in developing scalable, reusable UI components that
+      capture the user's attention through functionality and design. It's damn
+      fun writing clean, efficient code that not only optimizes performance, but
+      also provides an intuitive, seamless UX. I strive to leave the best first
+      impression. Always.
+    </p>
+    <p>
+      Currently, I spend my days creating software for LeanTaaS at this thing
+      called a job. For me, education never stops. There is so much to learn out
+      there, so much information to absorb, and not just in software
+      engineering. Whether it's honing my sudoku skills or effortlessly cooking
+      the perfect steak everytime, I am always looking to improve myself. If you
+      haven't already, try a growth mindset. I heard it's good for you.
+    </p>
   </section>
 );
 
