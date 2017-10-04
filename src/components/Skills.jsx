e388bafee52ab13841ddb6e15dd74fe58b479391
Brendan Mok <brendanmok@Brendans-MacBook-Pro.local>
2017-10-04 03:44:33 -0700

    Add alt to images

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -8,57 +8,57 @@ const Skills = () => (
         <img src="../public/coding.png" alt="Front End Icon" />
         <h3>Front End</h3>
         <ul>
-          <li><img src="../public/react.png" alt="" />React</li>
-          <li><img src="../public/redux.png" alt="" />Redux</li>
-          <li><img src="../public/angular.png" alt="" />Angular 2</li>
-          <li><img src="../public/typescript.png" alt="" />TypeScript</li>
-          <li><img src="../public/jquery.png" alt="" />jQuery</li>
-          <li><img src="../public/html5.png" alt="" />HTML5</li>
-          <li><img src="../public/css3.png" alt="" />CSS3</li>
-          <li><img src="../public/sass.png" alt="" />Sass</li>
-          <li><img src="../public/bootstrap.png" alt="" />Bootstrap</li>
+          <li><img src="../public/react.png" alt="React Icon" />React</li>
+          <li><img src="../public/redux.png" alt="Redux Icon" />Redux</li>
+          <li><img src="../public/angular.png" alt="Angular Icon" />Angular 2</li>
+          <li><img src="../public/typescript.png" alt="TypeScript Icon" />TypeScript</li>
+          <li><img src="../public/jquery.png" alt="jQUery Icon" />jQuery</li>
+          <li><img src="../public/html5.png" alt="HTML5 Icon" />HTML5</li>
+          <li><img src="../public/css3.png" alt="CSS3 Icon" />CSS3</li>
+          <li><img src="../public/sass.png" alt="Sass Icon" />Sass</li>
+          <li><img src="../public/bootstrap.png" alt="Bootstrap Icon" />Bootstrap</li>
         </ul>
       </header>
       <header>
         <img src="../public/server.png" alt="Server Icon" />
         <h3>Back End</h3>
         <ul>
-          <li><img src="../public/node.png" alt="" />Node</li>
-          <li><img src="../public/express.png" alt="" />Express</li>
-          <li><img src="../public/api.png" alt="" />RESTful APIs</li>
+          <li><img src="../public/node.png" alt="Node Icon" />Node</li>
+          <li><img src="../public/express.png" alt="Express Icon" />Express</li>
+          <li><img src="../public/api.png" alt="API Icon" />RESTful APIs</li>
         </ul>
       </header>
       <header>
         <img src="../public/database.png" alt="Database Icon" />
         <h3>Data Storage</h3>
         <ul>
-          <li><img src="../public/s3.png" alt="" />AWS S3</li>
-          <li><img src="../public/mongodb.png" alt="" />MongoDB</li>
-          <li><img src="../public/mysql.png" alt="" />MySQL</li>
+          <li><img src="../public/s3.png" alt="S3 Icon" />AWS S3</li>
+          <li><img src="../public/mongodb.png" alt="MongoDB Icon" />MongoDB</li>
+          <li><img src="../public/mysql.png" alt="MySQL Icon" />MySQL</li>
         </ul>
       </header>
       <header>
         <img src="../public/tools.png" alt="Tools Icon" />
         <h3>Tools</h3>
         <ul>
-          <li><img src="../public/webpack.png" alt="" />Webpack</li>
-          <li><img src="../public/gulp.png" alt="" />Gulp</li>
-          <li><img src="../public/heroku.jpg" alt="" />Heroku</li>
-          <li><img src="../public/digital_ocean.jpg" alt="" />Digital Ocean</li>
-          <li><img src="../public/babel.png" alt="" />Babel</li>
-          <li><img src="../public/postman.png" alt="" />Postman</li>
+          <li><img src="../public/webpack.png" alt="Webpack Icon" />Webpack</li>
+          <li><img src="../public/gulp.png" alt="Gulp Icon" />Gulp</li>
+          <li><img src="../public/heroku.jpg" alt="Heroku Icon" />Heroku</li>
+          <li><img src="../public/digital_ocean.jpg" alt="Digital Ocean Icon" />Digital Ocean</li>
+          <li><img src="../public/babel.png" alt="Babel Icon" />Babel</li>
+          <li><img src="../public/postman.png" alt="Postman Icon" />Postman</li>
         </ul>
       </header>
       <header>
         <img src="../public/responsive.png" alt="General Icon" />
         <h3>General</h3>
         <ul>
-          <li><img src="../public/github.png" alt="" />Git/Github</li>
-          <li><img src="../public/terminal.png" alt="" />Command Line</li>
-          <li><img src="../public/chrome.png" alt="" />Chrome DevTools</li>
-          <li><img src="../public/tdd.png" alt="" />TDD</li>
-          <li><img src="../public/vscode.png" alt="" />VSCode</li>
-          <li><img src="../public/yarn.jpeg" alt="" />Yarn</li>
+          <li><img src="../public/github.png" alt="Github Icon" />Git/Github</li>
+          <li><img src="../public/terminal.png" alt="Terminal Icon" />Command Line</li>
+          <li><img src="../public/chrome.png" alt="Chrome Icon" />Chrome DevTools</li>
+          <li><img src="../public/tdd.png" alt="TDD Icon" />TDD</li>
+          <li><img src="../public/vscode.png" alt="VSCode Icon" />VSCode</li>
+          <li><img src="../public/yarn.jpeg" alt="Yarn Icon" />Yarn</li>
         </ul>
       </header>
     </div>
