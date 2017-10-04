d0b80191d3326b3fa851b983c0ee0f6df67fc565
Brendan Mok <brendanmok@Brendans-MacBook-Pro.local>
2017-10-04 11:05:03 -0700

    Finish timeline section

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -7,9 +7,26 @@ const Timeline = () => (
       <div id="timeline" />
       <div className="timeline-block">
         <div className="timeline-content">
-          <h4>LeanTaaS</h4>
-          <h5>Software Engineer</h5>
-          <p>TBD...</p>
+          <h3>LeanTaaS</h3>
+          <h4>Software Engineer</h4>
+          <p>Tbd...</p>
+        </div>
+      </div>
+      <div className="timeline-block">
+        <div className="timeline-content">
+          <h3>Hack Reactor</h3>
+          <h4>Student</h4>
+          <p>
+            A 12-week full stack software engineering immersive. We learned
+            JavaScript, computer science fundamentals, and how to build
+            production-grade web applications, mimicking industry standards and
+            workflow as close as possible.
+          </p>
+        </div>
+      </div>
+      <div className="timeline-block">
+        <div className="timeline-content">
+          <h3>"Hello, World!"</h3>
         </div>
       </div>
     </div>
